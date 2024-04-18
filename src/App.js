373ee3c15f034c157bbfbc0b84ee45020ba8c0ff
aizirek jusupbaeva <aizirekjusupbaeva@gmail.com>
2024-04-18 15:39:59 +0600

    made home page and styled

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
      <Header/>
       <Routes>
         <Route path='/about' element={<About/>}/>
-        <Route path='/home' element={<Home/>}/>
+        <Route path='/' element={<Home/>}/>
         <Route path='/mans' element={<Mans/>}/>
         <Route path='/womans' element={<Womans/>}/>
       </Routes>
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
-
+import './Home.css'
 
 const Home = () => {
 	return (
-		<div>
-			
+		<div className='home'>
+			<div className="container">
+                <div className="home-flex">
+                    <h1 className='regestration'>Sign In Now!</h1>
+                    <h2> To order, please sign in</h2>
+                    <div className="flex-forum">
+                        <input className='forum' type="email" name="" id="" />
+                        <input className='forum' type="password" name="" id="" />
+                        <button className='btn'>Sign in</button>
+                    </div>
+                    <br />
+                    <br />
+                    <p>if you need help click "About"</p>
+                </div>
+            </div>
 		</div>
 	)
 }
